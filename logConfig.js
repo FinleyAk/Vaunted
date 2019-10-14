c2fa005f5fb131504887c1434dfdfde6d1edc33c
Kalum <kalumh@live.co.uk>
2019-10-15 00:17:14 +0100

    Code Quality  Fix

--- logConfig.js
+++ logConfig.js
@@ -2,78 +2,78 @@ module.exports = {
     levels: {
         TRACE: {
             value: 5001,
-            colour: 'grey'
+            colour: "grey"
         },
         DEFAULT: {
             value: 5001,
-            colour: 'grey'
+            colour: "grey"
         },
         DEBUG: {
             value: 10001,
-            colour: 'blue'
+            colour: "blue"
         },
         INFO: {
             value: 20001,
-            colour: 'cyan'
+            colour: "cyan"
         },
         WARN: {
             value: 30001,
-            colour: 'green'
+            colour: "green"
         },
         ERROR: {
             value: 40001,
-            colour: 'yellow'
+            colour: "yellow"
         },
         FATAL: {
             value: 50001,
-            colour: 'red'
+            colour: "red"
         }
     },
     appenders: {
         out: {
-            type: 'stdout',
+            type: "stdout",
             layout: {
-                type: 'pattern',
-                pattern: '%[[%d{hh:mm:ss}][%-5p] [%-32.32c]%] %m',
+                type: "pattern",
+                pattern: "%[[%d{hh:mm:ss}][%-5p] [%-32.32c]%] %m",
             }
         },
         outFilter: {
-            type: 'logLevelFilter',
-            appender: 'out',
-            level: process.argv[2] || 'info'
+            type: "logLevelFilter",
+            appender: "out",
+            level: process.argv[2] || "info"
         },
         debugLog: {
-            type: 'dateFile',
-            filename: './logs/debug/',
-            pattern: 'dd-MM-yyyy.log',
+            type: "dateFile",
+            filename: "./logs/debug/",
+            pattern: "dd-MM-yyyy.log",
             alwaysIncludePattern: true,
             keepFileExt: true,
             layout: {
-                type: 'pattern',
-                pattern: '[%d{hh:mm:ss}][%-5p] [%-32.32c] %m'
+                type: "pattern",
+                pattern: "[%d{hh:mm:ss}][%-5p] [%-32.32c] %m"
             }
         },
         errorLog: {
-            type: 'dateFile',
-            filename: './logs/error/',
-            pattern: 'dd-MM-yyyy.log',
+            type: "dateFile",
+            filename: "./logs/error/",
+            pattern: "dd-MM-yyyy.log",
             alwaysIncludePattern: true,
             keepFileExt: true,
             layout: {
-                type: 'pattern',
-                pattern: '[%d{hh:mm:ss}][%-5p] [%-32.32c] %m'
+                type: "pattern",
+                pattern: "[%d{hh:mm:ss}][%-5p] [%-32.32c] %m"
             }
         },
         errorFilter: {
-            type: 'logLevelFilter',
-            appender: 'errorLog',
-            level: 'warn'
+            type: "logLevelFilter",
+            appender: "errorLog",
+            level: "warn"
         }
     },
     categories: {
         default: {
-            appenders: ['outFilter', 'debugLog', 'errorFilter'],
-            level: 'all'
+            appenders: ["outFilter", "debugLog", "errorFilter"],
+            level: "all"
         }
     }
 };
